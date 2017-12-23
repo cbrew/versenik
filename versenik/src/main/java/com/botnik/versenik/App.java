fac15fd7624d5a05d8ed1db646610bfccb130104
Chris Brew <cbrew@acm.org>
2017-12-23 16:27:54 -0500

    basic java program

---
+++ versenik/src/main/java/com/botnik/versenik/App.java
@@ -0,0 +1,13 @@
+package com.botnik.versenik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
